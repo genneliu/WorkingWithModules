41e9ea1527463c1bad046ad60e38fcd10f27f6f8
Genne Liu <genneliu@gmail.com>
2021-10-18 09:09:02 -0700

    module2

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+//iffy wont share anything outside of scope
+(function(){
+    function getSessions(){
+        return new Promise(function(resolve, reject){
+          var oReq = new XMLHttpRequest();
+          oReq.onload = function (e) {
+            resolve(e.target.response);
+          };
+          oReq.open('GET', 'sessions.json', true);
+          oReq.responseType = 'json';
+          oReq.send();
+        })
+      }
+    
+      
+      function render() {
+        var list = document.querySelector('#sessions');
+        if (!list) return;
+        list.innerHTML = sessionTemplate(data.listItems);
+      };
+      
+      
+      var data = {
+        listItems: []
+      };
+      
+      getSessions()
+        .then((sessions)=>{
+          console.log('promises!')
+          data.listItems = sessions;
+          render();
+        });
+}())
+//iffy 
+
---
+++ public/js/sessionrepository.js
@@ -0,0 +1,22 @@
+const sessionURL = 'sessions.json';
+
+//store session list
+let sessionList = [];
+
+function getSessions(){
+    return new Promise(function(resolve, reject){
+      var oReq = new XMLHttpRequest();
+      oReq.onload = function (e) {
+        //instead of resolving and sending target response, save session list
+        sessionList = e.target.response;
+        resolve(e.target.response);
+      };
+      oReq.open('GET', sessionURL, true);
+      oReq.responseType = 'json';
+      oReq.send();
+    })
+  }
+
+  //takes list
+  //export default - 
+  export {getSessions};
